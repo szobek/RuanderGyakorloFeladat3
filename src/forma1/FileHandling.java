424c79dfe831aec0b151264467fbf84d606bc8e0
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-05-05 15:03:49 +0200

    create classes

---
+++ src/forma1/FileHandling.java
@@ -0,0 +1,8 @@
+package forma1;
+
+public class FileHandling {
+public void readFile(String fileName, String delimiter) {
+	
+	
+}
+}
---
+++ src/forma1/Pilota.java
@@ -0,0 +1,9 @@
+package forma1;
+
+public class Pilota {
+private String name;
+private String birthDate;
+private String country;
+private int num;
+private boolean isActive;
+}
